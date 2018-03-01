c6d3e30aca1e71dc0259c949eb1abc9db25df4f2
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-03-01 17:28:27 -0600

    played around with jquery to append at paragraph click

--- DojoAssingments/WebFundamentals/Contact Assingment/jquery.js
+++ DojoAssingments/WebFundamentals/Contact Assingment/jquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    alert('jquery');
+    //alert('jquery');
     $('#submit').click(function(){
         var fname = $('#first_name').val(); //grab the input forms
         var lname = $('#last_name').val();
@@ -7,7 +7,9 @@ $(document).ready(function(){
         $('#rightside').append(`<div class="info"><h1>${fname} ${lname}</h1> <p>Click for description</p></div>`);
         return false
         });
+
         $('#rightside').on('click', 'p', function(){
-            alert('you clicked paragraph');
-    });
+            
+              alert('you clicked paragraph');
+        });
 });
