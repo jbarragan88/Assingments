a845be8af4323de51e16dfa8ba5c6499cb000f95
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-03-01 16:46:56 -0600

    New Assingment

---
+++ DojoAssingments/WebFundamentals/Contact Assingment/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    alert('jquery');
+    $('#submit').click(function(){
+        var fname = $('#first_name').val(); //grab the input forms
+        var lname = $('#last_name').val();
+        var descript = $('#description').val();
+        $('#rightside').append(`<div class="info"><h1>${fname} ${lname}</h1> <p>Click for description</p></div>`);
+        return false
+        });
+        $('#rightside').on('click', 'p', function(){
+            alert('you clicked paragraph');
+    });
+});
