5e7c87d74700f9a0c62494bc3d59fb164fae58f7
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-03-05 14:29:45 -0600

    New Python Assingments

---
+++ DojoAssingments/Python/SL_Find_and_replace.py
@@ -0,0 +1,4 @@
+words = "It's thanksgiving day. It's my birthday, too!"
+new_str = words.replace('day', 'month')
+print words.find('day')
+print new_str
---
+++ DojoAssingments/Python/SL_first_last.py
@@ -0,0 +1,5 @@
+x = ["hello",2,54,-2,7,12,98,"world"]
+newli = [x[0], x[len(x)-1]] 
+print x[0]
+print x[len(x)-1]
+print newli
