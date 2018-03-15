dafe76c6b0f26abf03c3576c6dcb48d14e5af04c
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-03-14 19:14:08 -0500

    Email Validation Assingment Success Works

--- DojoAssingments/Python/flask_MySQL/assingment/email_validation/server.py
+++ DojoAssingments/Python/flask_MySQL/assingment/email_validation/server.py
@@ -8,4 +8,25 @@ mysql = MySQLConnector(app,'emails')
 def index():
     return render_template('index.html')
 
+@app.route('/validate', methods=['POST'])
+def validation():
+    data = {
+            'email': request.form['email']
+            }
+    query = "SELECT email FROM users WHERE email LIKE :email"
+    print mysql.query_db(query,data)
+    if mysql.query_db(query,data) == []:
+        query1 = "INSERT INTO users (email) VALUES (:email)"
+        mysql.query_db(query1,data)
+        return redirect('/yes')
+    else:
+        return redirect('/no')
+
+@app.route('/yes')
+def yess():
+    return render_template('success.html')
+
+@app.route('/no', methods=['POST'])
+def noo():
+    return render_template('invalid.html')
 app.run(debug=True)
