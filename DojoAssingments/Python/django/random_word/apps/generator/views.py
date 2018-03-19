93d23a9a415927b61a7804c2a999e216e28f8549
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-03-19 09:05:18 -0500

    Reandom Word Generator Assingment
    creates 14 random characters

--- DojoAssingments/Python/django/random_word/apps/generator/views.py
+++ DojoAssingments/Python/django/random_word/apps/generator/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 
 # Create your views here.
+def start():
+    request.session['attempt'] = 0
 def index(request):
     print "made it to little url index"
-    return render(request, 'index.html')
+    request.session['attempt'] += 1
+    random = {
+        "random1" : get_random_string(length=14)
+    }
+    return render(request, 'index.html', random)
 
 def reset(request):
+    request.session['attempt'] = 0
     print "made it to little url reset"
-    return render(request, 'index.html')
+    return redirect('/')
--- DojoAssingments/Python/django/random_word/random_word/urls.py
+++ DojoAssingments/Python/django/random_word/random_word/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('apps.generator.urls')),
-    url(r'^render_template/reset$', include('apps.generator.urls')),
+    url(r'^', include('apps.generator.urls')),
+    url(r'^render_template/reset', include('apps.generator.urls')),
 ]
