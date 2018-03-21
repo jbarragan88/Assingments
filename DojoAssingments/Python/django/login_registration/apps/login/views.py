ec8230d3cb1ae89a252952f9bb40034bb6bd56b9
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-03-21 10:02:34 -0500

    Django Login Registration

--- DojoAssingments/Python/django/login_registration/apps/login/views.py
+++ DojoAssingments/Python/django/login_registration/apps/login/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 
+from django.contrib import messages
+import bcrypt
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
