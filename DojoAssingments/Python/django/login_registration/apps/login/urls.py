ea19f9bb63ca9ee411d92eb3ec6e0a3b75dcf045
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-03-21 08:17:31 -0500

    Django Login Registration
    Added routes to redirect to loggein

--- DojoAssingments/Python/django/login_registration/apps/login/urls.py
+++ DojoAssingments/Python/django/login_registration/apps/login/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^registration$', views.registration, name="registration"),
+    url(r'^login$', views.login, name="login"),
+    url(r'^loggedin$', views.loggedin, name="loggedin"),
 ]
--- DojoAssingments/Python/django/login_registration/apps/login/views.py
+++ DojoAssingments/Python/django/login_registration/apps/login/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def registration(request):
+    return redirect('/loggedin')
+
+def login(request):
+    return redirect('/loggedin')
+
+def loggedin(request):
+    return render(request, 'loggedin.html')
