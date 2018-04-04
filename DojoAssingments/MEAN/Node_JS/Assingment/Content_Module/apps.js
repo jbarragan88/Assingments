39cd6180fed3648aff3a1cd76e5f91fffd3cbec3
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-04-04 17:53:41 -0500

    Optional Assingment Content Module
    able to responde with stylesheets
    and images

--- DojoAssingments/MEAN/Node_JS/Assingment/Content_Module/apps.js
+++ DojoAssingments/MEAN/Node_JS/Assingment/Content_Module/apps.js
@@ -5,15 +5,11 @@ const   fs = require('fs');
 // NOTE!!!  The '.' in the filepath below just refers to the location of the current file, in this case
 // the file is app.js.  Thus the path './static.js' just refers to a file called static.js
 const static_contents = require('./module/module.js')();
-//
 //creating a server
 server = http.createServer(function (request, response){
   //static_contents(request, response);  //this will serve all static files automatically
   static_contents.index(request, response, fs);
 });
-console.log(static_contents);
-console.log(server.request);
-//static_contents.index(server.request);
 
 server.listen(8000);
 console.log("Running in localhost at port 8000");
--- DojoAssingments/MEAN/Node_JS/Assingment/Content_Module/module/module.js
+++ DojoAssingments/MEAN/Node_JS/Assingment/Content_Module/module/module.js
@@ -1,12 +1,43 @@
 module.exports = function (request, responce){
     return {
       index: function(request, response, fs) { 
-          var responding = response.writeHead(200, {'Content-type': 'text/html'});
-            fs.readFile('views/index.html', 'utf8', function (errors, contents) {
-            response.write(contents); 
-            response.end();
+          console.log(request.url);
+          var url = request.url
+            if(url.indexOf('css') > -1){
+              var the_url = request.url;
+                var responding = response.writeHead(200, {'Content-type': 'text/css'});
+                fs.readFile('style'+ the_url , 'utf8', function (errors, contents) {
+                response.write(contents); 
+                response.end();
+              
+              console.log('hello')
+              return responding;
+            });
+          }
+          else if(url.indexOf('img') > -1){
+              var the_url = request.url;
+              var responding = response.writeHead(200, {'Content-type': 'image/jpg'});
+                fs.readFile('images'+ the_url ,function (errors, contents) {
+                response.write(contents); 
+                response.end();
+              
+              console.log('hello')
+              return responding;
+            });
+          }
+          else{
+            var the_url = request.url;
+              var responding = response.writeHead(200, {'Content-type': 'text/html'});
+              fs.readFile('views'+ the_url +'.html', 'utf8', function (errors, contents) {
+              response.write(contents); 
+              response.end();
+            
+            console.log(responding)
             return responding;
-          });    
+          });
+          }
+          
+          
       }
     }
   };
