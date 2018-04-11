191234c7f9756bbad59edea00286b10cf2cc38e2
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-04-11 10:27:10 -0500

    Mongoose Assingment Message Board Finished

--- DojoAssingments/MEAN/Mongoose/Assingment/Message_Board/server.js
+++ DojoAssingments/MEAN/Mongoose/Assingment/Message_Board/server.js
@@ -41,13 +41,12 @@ app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 // Routes
 
+//Saving the Message 
 app.post('/message', function(req, res) {
     console.log("POST DATA", req.body);
-    // create a new User with the name and age corresponding to those from req.body
+    // create a new Message with the name and message input
     var message = new Message({name: req.body.name, message: req.body.message});
-    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
     message.save(function(err) {
-      // if there is an error console.log that something went wrong!
       if(err) {
         console.log('something went wrong');
         res.redirect('/');
@@ -61,22 +60,11 @@ app.post('/message', function(req, res) {
 // Root Request
 app.get('/', function(req, res) {
     // This is where we will retrieve the dogs from the database and include them in the view page we will be rendering.
-    Message.find({}, function(err, messages) { 
-        if(err){
-            console.log("Wasn't able to retrive shit from the database. I'm Done...")
-            res.redirect('/')
-        }
-        else{
-            console.log(messages);
-            var message_array = messages;
-            Comment.find({})
-            .populate('comments')
-            .exec(function(err, comment) {
-                console.log("results from comments:", comment);
-                res.render('index', {comment: comment, messages: message_array});
-            });
-        }
-      })
+    Message.find({})
+        .populate('comments')
+        .exec(function(err, message) {
+            res.render('index', {messages: message});
+        });
 })
 app.post('/comment/:id', function(req, res) {
     // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
@@ -88,10 +76,10 @@ app.post('/comment/:id', function(req, res) {
         comment._post = message._id;
         comment.name = req.body.name;
         comment.text = req.body.comment;
+        message[0].comments.push(comment);
         comment.save(function(err){
             console.log("message!!!:", message);
             console.log("FIRST COMMENT:", comment);
-            message[0].comments.push(comment);
             message[0].save(function (err){
                 if(err){
                     console.log("Couldn't add comment" )
@@ -102,69 +90,9 @@ app.post('/comment/:id', function(req, res) {
                 }
             })
         })
-        
-        //console.log(dog);
-        //var id = req.params.id;
-        //res.redirect('/dogs/'+id)
+      })
+})
 
-
-      })
-})
-app.get('/dogs/:id', function(req, res) {
-    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
-    Dog.find({_id: req.params.id}, function(err, dogs) {
-        // This is the method that finds all of the users from the database
-        // Notice how the first parameter is the options for what to find and the second is the
-        //   callback function that has an error (if any) and all of the users
-        // Keep in mind that everything you want to do AFTER you get the users from the database must
-        //   happen inside of this callback for it to be synchronous 
-        // Make sure you handle the case when there is an error, as well as the case when there is no error
-        if(err){
-            console.log("Wasn't able to retrive shit from the database. I'm Done...")
-            res.redirect('/')
-        }
-        else{
-            console.log(dogs);
-            var dogs_array = dogs;
-            res.render('view', {dog: dogs_array})
-        }
-      })
-})
-app.get('/dogs/edit/:id', function(req, res) {
-    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
-    Dog.find({_id: req.params.id}, function(err, dogs) {
-        // This is the method that finds all of the users from the database
-        // Notice how the first parameter is the options for what to find and the second is the
-        //   callback function that has an error (if any) and all of the users
-        // Keep in mind that everything you want to do AFTER you get the users from the database must
-        //   happen inside of this callback for it to be synchronous 
-        // Make sure you handle the case when there is an error, as well as the case when there is no error
-        if(err){
-            console.log("Wasn't able to retrive shit from the database. I'm Done...")
-            res.redirect('/')
-        }
-        else{
-            console.log(dogs);
-            var dogs_array = dogs;
-            res.render('edit', {dog: dogs_array})
-        }
-      })
-})
-app.get('/dogs/destroy/:id', function(req, res) {
-    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
-    Dog.find({_id: req.params.id}, function(err, dogs) {
-        Dog.remove({_id: req.params.id}, function(err){
-            if(err){
-                console.log("Wasn't able to delete shit from the database. I'm Done...")
-                res.redirect('/')
-            }
-            else{
-                console.log('Killing one dog at a time')
-                res.redirect('/')
-            }
-        })
-      })
-})
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
