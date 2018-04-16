60f87f1865e606b00e448b01c901d1e8229e4af9
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-04-16 08:50:25 -0500

    Angular Practice Client Events

--- DojoAssingments/MEAN/Angular/Assingment/Hello_Angular/myFirstAngularApp/src/app/app.component.ts
+++ DojoAssingments/MEAN/Angular/Assingment/Hello_Angular/myFirstAngularApp/src/app/app.component.ts
@@ -23,4 +23,17 @@ export class AppComponent implements OnInit {
       this.tasks = data['data'];
     })
   }
+
+  onButtonClick(): void {
+    console.log(`Click event is working`);
+  }
+  onButtonClickParam(num: Number): void { 
+    console.log(`Click event is working with num param: ${num}`);
+  }
+  onButtonClickParams(num: Number, str: String): void { 
+      console.log(`Click event is working with num param: ${num} and str param: ${str}`);
+  }
+  onButtonClickEvent(event: any): void { 
+      console.log(`Click event is working with event: `, event);
+  }
 }
