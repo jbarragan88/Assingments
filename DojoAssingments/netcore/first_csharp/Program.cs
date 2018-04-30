3f3a7a63e755df58b5c9a541b5eac1d3e52ef9e4
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-04-30 10:30:44 -0500

    C# Practice Reading

--- DojoAssingments/netcore/first_csharp/Program.cs
+++ DojoAssingments/netcore/first_csharp/Program.cs
@@ -6,7 +6,20 @@ namespace first_csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+
+        // loop from 1 to 5 including 5
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        // loop from 1 to 5 excluding 5
+        for (int i = 1; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
         }
     }
 }
