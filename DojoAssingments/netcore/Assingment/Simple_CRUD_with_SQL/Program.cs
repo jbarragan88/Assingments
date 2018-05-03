1e1d979b34fb521c28a39de4b665eae25023c017
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-05-03 17:42:04 -0500

    C# Assingment Simple CRUD with MySQL
    in progress

---
+++ DojoAssingments/netcore/Assingment/Simple_CRUD_with_SQL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DbConnection;
+using System.Collections.Generic;
+
+namespace Simple_CRUD_with_SQL
+{
+    class Program
+    {
+        static public void DisplayAllUsers()
+        {
+            List<Dictionary<string, object>> query = DbConnector.Query("SELECT * FROM Users;");
+            foreach(var entry in query)
+            {
+               foreach(var item in entry)
+               {
+                   Console.WriteLine(item.Key + " - " + item.Value);
+               }
+            }
+}
+        static void Main(string[] args)
+        {
+            // string InputLine = Console.ReadLine();
+            // Console.WriteLine("The console string: "+ InputLine);
+            DisplayAllUsers();
+        }
+    }
+}
