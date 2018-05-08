524dc5120421a40b5b4cfc64b83bc6bf31a46da9
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-05-07 20:25:46 -0500

    C# Assingment Portfoli
    incomplete

---
+++ DojoAssingments/netcore/Assingment/Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("/projects")]
+        public IActionResult Projects()
+        {
+            return View("Projects");
+        }
+        [HttpGet]
+        [Route("/contact")]
+        public IActionResult Contact()
+        {
+            return View("Contact");
+        }
+    }
+}
