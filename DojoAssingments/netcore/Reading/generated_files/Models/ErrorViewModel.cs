7f34fbc59174c6382df45cb61c809c37ba867808
jbarragan88 <jonathanbarragan88@yahoo.com>
2018-05-09 11:20:06 -0500

    C# Reading Rendering Views

---
+++ DojoAssingments/netcore/Reading/generated_files/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace generated_files.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
